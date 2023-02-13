7561a5f0ed86b57b77c21dbea6300052703ccd22
maxday <maxime.david@datadoghq.com>
2023-02-12 22:48:55 -0500

    add quarkus to deployer

--- function-deployer/app.js
+++ function-deployer/app.js
@@ -74,6 +74,11 @@ const functionsToDeploy = [
         handler:    'bootstrap',
         path:       'rust_on_provided_al2',
     },
+    {
+        runtime:    'provided.al2',
+        handler:    'bootstrap',
+        path:       'quarkus_native_on_provided_al2',
+    },
 ];
 
 const deleteFunction = async (client, functionName) => {
