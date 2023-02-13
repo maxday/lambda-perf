8a7970ec8bb0bff4fcd150667a1b37fb798ec785
Maxime David <maxime.david@datadoghq.com>
2023-02-12 23:54:34 -0500

    reset indent to 2

--- function-invoker/app.js
+++ function-invoker/app.js
@@ -1,8 +1,8 @@
 const {
-    LambdaClient,
-    InvokeCommand,
-    UpdateFunctionConfigurationCommand,
-    ListVersionsByFunctionCommand,
+  LambdaClient,
+  InvokeCommand,
+  UpdateFunctionConfigurationCommand,
+  ListVersionsByFunctionCommand,
 } = require("@aws-sdk/client-lambda");
 
 const REGION = process.env.AWS_REGION;
@@ -11,87 +11,86 @@ const NB_INVOKE = 10;
 const DELAY = 10000;
 
 const invokeFunction = async (client, functionName) => {
-    const params = {
-        FunctionName: functionName,
-    };
-    try {
-        const command = new InvokeCommand(params);
-        await client.send(command);
-        console.log(`function ${functionName} invoked`);
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
+  const params = {
+    FunctionName: functionName,
+  };
+  try {
+    const command = new InvokeCommand(params);
+    await client.send(command);
+    console.log(`function ${functionName} invoked`);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 const updateFunction = async (client, functionName) => {
-    const params = {
-        FunctionName: functionName,
-        Environment: {
-            Variables: { coldStart: `${Math.random()}` },
-        },
-    };
-    try {
-        const command = new UpdateFunctionConfigurationCommand(params);
-        await client.send(command);
-        console.log(`function ${functionName} updated`);
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
+  const params = {
+    FunctionName: functionName,
+    Environment: {
+      Variables: { coldStart: `${Math.random()}` },
+    },
+  };
+  try {
+    const command = new UpdateFunctionConfigurationCommand(params);
+    await client.send(command);
+    console.log(`function ${functionName} updated`);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function isSnapStart(runtime) {
-    return runtime.toLowerCase().includes("snapstart");
+  return runtime.toLowerCase().includes("snapstart");
 }
 
 exports.handler = async (event, context) => {
-    try {
-        const runtime = context.clientContext.runtime;
-        const functionName = `${PREFIX}${runtime}`;
-        const lambdaClient = new LambdaClient({ region: REGION });
+  try {
+    const runtime = context.clientContext.runtime;
+    const functionName = `${PREFIX}${runtime}`;
+    const lambdaClient = new LambdaClient({ region: REGION });
 
-        let versions = [];
-        if (isSnapStart(runtime)) {
-            versions = await getFunctionVersions(lambdaClient, functionName);
-        }
-        for (let i = 0; i < NB_INVOKE; ++i) {
-            if (isSnapStart(runtime)) {
-                let functionNameWithVersion = `${functionName}:${versions[i]}`;
-                console.log(`invoking function ${functionNameWithVersion}`);
-                await invokeFunction(lambdaClient, functionNameWithVersion);
-            } else {
-                await invokeFunction(lambdaClient, functionName);
-                await updateFunction(lambdaClient, functionName);
-                await delay(DELAY);
-            }
-        }
-        return {
-            statusCode: 200,
-            body: JSON.stringify("success"),
-        };
-    } catch (_) {
-        throw "failure";
+    let versions = [];
+    if (isSnapStart(runtime)) {
+      versions = await getFunctionVersions(lambdaClient, functionName);
     }
+    for (let i = 0; i < NB_INVOKE; ++i) {
+      if (isSnapStart(runtime)) {
+        let functionNameWithVersion = `${functionName}:${versions[i]}`;
+        console.log(`invoking function ${functionNameWithVersion}`);
+        await invokeFunction(lambdaClient, functionNameWithVersion);
+      } else {
+        await invokeFunction(lambdaClient, functionName);
+        await updateFunction(lambdaClient, functionName);
+        await delay(DELAY);
+      }
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify("success"),
+    };
+  } catch (_) {
+    throw "failure";
+  }
 };
 
 // get all function versions
 const getFunctionVersions = async (client, functionName) => {
-    const params = {
-        FunctionName: functionName,
-    };
-    try {
-        const command = new ListVersionsByFunctionCommand(params);
-        const response = await client.send(command);
-        //filter all versions except $LATEST in a string array
-        return response.Versions.filter(
-            (version) => version.Version !== "$LATEST"
-        ).map((version) => version.Version);
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
+  const params = {
+    FunctionName: functionName,
+  };
+  try {
+    const command = new ListVersionsByFunctionCommand(params);
+    const response = await client.send(command);
+    //filter all versions except $LATEST in a string array
+    return response.Versions.filter(
+      (version) => version.Version !== "$LATEST"
+    ).map((version) => version.Version);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
-
--- report-log-processor/app.js
+++ report-log-processor/app.js
@@ -5,49 +5,49 @@ const dynamoDb = new DocumentClient();
 const TABLE = "report-log";
 
 exports.handler = async (input, context) => {
-    var payload = Buffer.from(input.awslogs.data, "base64");
-    var result = zlib.gunzipSync(payload);
-    result = JSON.parse(result.toString());
-    const fromLambda = result.logGroup.replace("/aws/lambda/", "");
-    for (const singleEvent of result.logEvents) {
-        try {
-            const reportLogRegex =
-                /REPORT RequestId: (?<requestId>[a-z0-9\-]+)\s*Duration: (?<durationTime>[0-9\.]+) ms\s*Billed Duration: (?<billedDurationTime>[0-9\.]+) ms\s*Memory Size: (?<memorySize>[0-9\.]+) MB\s*Max Memory Used: (?<maxMemoryUsed>[0-9\.]+) MB\s*(Init Duration: (?<initDuration>[0-9\.]+) ms\s*)?\s*(Restore Duration: (?<restoreDuration>[0-9\.]+) ms\s*Billed Restore Duration: (?<billedRestoreDuration>[0-9\.]+) ms\s*)?/g;
-            const match = reportLogRegex.exec(singleEvent.message);
-            if (match) {
-                const {
-                    requestId,
-                    durationTime,
-                    billedDurationTime,
-                    memorySize,
-                    maxMemoryUsed,
-                    initDuration,
-                    restoreDuration,
-                    billedRestoreDuration,
-                } = match.groups;
-                const item = {
-                    requestId: requestId,
-                    lambda: fromLambda,
-                    duration: durationTime,
-                    billedDuratation: billedDurationTime,
-                    memorySize: memorySize,
-                    maxMemoryUsed: maxMemoryUsed,
-                    initDuration: initDuration ?? restoreDuration,
-                    restoreDuration: restoreDuration,
-                    billedRestoreDuration: billedRestoreDuration,
-                };
-                await dynamoDb
-                    .put({
-                        TableName: TABLE,
-                        Item: item,
-                    })
-                    .promise();
-                console.log("item inserted");
-            }
-        } catch (e) {
-            console.error(e);
-            context.fail();
-        }
+  var payload = Buffer.from(input.awslogs.data, "base64");
+  var result = zlib.gunzipSync(payload);
+  result = JSON.parse(result.toString());
+  const fromLambda = result.logGroup.replace("/aws/lambda/", "");
+  for (const singleEvent of result.logEvents) {
+    try {
+      const reportLogRegex =
+        /REPORT RequestId: (?<requestId>[a-z0-9\-]+)\s*Duration: (?<durationTime>[0-9\.]+) ms\s*Billed Duration: (?<billedDurationTime>[0-9\.]+) ms\s*Memory Size: (?<memorySize>[0-9\.]+) MB\s*Max Memory Used: (?<maxMemoryUsed>[0-9\.]+) MB\s*(Init Duration: (?<initDuration>[0-9\.]+) ms\s*)?\s*(Restore Duration: (?<restoreDuration>[0-9\.]+) ms\s*Billed Restore Duration: (?<billedRestoreDuration>[0-9\.]+) ms\s*)?/g;
+      const match = reportLogRegex.exec(singleEvent.message);
+      if (match) {
+        const {
+          requestId,
+          durationTime,
+          billedDurationTime,
+          memorySize,
+          maxMemoryUsed,
+          initDuration,
+          restoreDuration,
+          billedRestoreDuration,
+        } = match.groups;
+        const item = {
+          requestId: requestId,
+          lambda: fromLambda,
+          duration: durationTime,
+          billedDuratation: billedDurationTime,
+          memorySize: memorySize,
+          maxMemoryUsed: maxMemoryUsed,
+          initDuration: initDuration ?? restoreDuration,
+          restoreDuration: restoreDuration,
+          billedRestoreDuration: billedRestoreDuration,
+        };
+        await dynamoDb
+          .put({
+            TableName: TABLE,
+            Item: item,
+          })
+          .promise();
+        console.log("item inserted");
+      }
+    } catch (e) {
+      console.error(e);
+      context.fail();
     }
-    context.succeed();
+  }
+  context.succeed();
 };
