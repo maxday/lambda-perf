f2661e8fa3429ac3599a676086949fc465ea7ccb
Maxime David <maxime.david@datadoghq.com>
2023-02-12 23:36:45 -0500

    add prettier

--- function-invoker/app.js
+++ function-invoker/app.js
@@ -1,7 +1,7 @@
 const {
-  LambdaClient,
-  InvokeCommand,
-  UpdateFunctionConfigurationCommand,
+    LambdaClient,
+    InvokeCommand,
+    UpdateFunctionConfigurationCommand,
 } = require("@aws-sdk/client-lambda");
 
 const REGION = process.env.AWS_REGION;
@@ -10,52 +10,52 @@ const NB_INVOKE = 10;
 const DELAY = 10000;
 
 const invokeFunction = async (client, functionName) => {
-  const params = {
-    FunctionName: `${PREFIX}${functionName}`,
-  };
-  try {
-    const command = new InvokeCommand(params);
-    await client.send(command);
-    console.log(`function ${functionName} invoked`);
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+    const params = {
+        FunctionName: `${PREFIX}${functionName}`,
+    };
+    try {
+        const command = new InvokeCommand(params);
+        await client.send(command);
+        console.log(`function ${functionName} invoked`);
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
 };
 
 const updateFunction = async (client, functionName) => {
-  const params = {
-    FunctionName: `${PREFIX}${functionName}`,
-    Environment: {
-      Variables: { coldStart: `${Math.random()}` },
-    },
-  };
-  try {
-    const command = new UpdateFunctionConfigurationCommand(params);
-    await client.send(command);
-    console.log(`function ${functionName} updated`);
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
+    const params = {
+        FunctionName: `${PREFIX}${functionName}`,
+        Environment: {
+            Variables: { coldStart: `${Math.random()}` },
+        },
+    };
+    try {
+        const command = new UpdateFunctionConfigurationCommand(params);
+        await client.send(command);
+        console.log(`function ${functionName} updated`);
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
 };
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 exports.handler = async (event, context) => {
-  try {
-    const runtime = context.clientContext.runtime;
-    const lambdaClient = new LambdaClient({ region: REGION });
-    for (let i = 0; i < NB_INVOKE; ++i) {
-      await invokeFunction(lambdaClient, runtime);
-      await updateFunction(lambdaClient, runtime);
-      await delay(DELAY);
+    try {
+        const runtime = context.clientContext.runtime;
+        const lambdaClient = new LambdaClient({ region: REGION });
+        for (let i = 0; i < NB_INVOKE; ++i) {
+            await invokeFunction(lambdaClient, runtime);
+            await updateFunction(lambdaClient, runtime);
+            await delay(DELAY);
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify("success"),
+        };
+    } catch (_) {
+        throw "failure";
     }
-    return {
-      statusCode: 200,
-      body: JSON.stringify("success"),
-    };
-  } catch (_) {
-    throw "failure";
-  }
 };
--- s3-uploader/app.js
+++ s3-uploader/app.js
@@ -1,40 +1,42 @@
-const fs = require('fs');
-const childProcess = require('child_process');
+const fs = require("fs");
+const childProcess = require("child_process");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const REGION = process.env.AWS_REGION;
 
 (async () => {
     const runtimes = [
-        'nodejs12x', 
-        'nodejs14x', 
-        'nodejs16x', 
-        'nodejs18x', 
-        'python37',
-        'python38',
-        'python39',
-        'dotnetcore31',
-        'dotnet6',
-        'go1x',
-        'java11',
-        'java8',
-        'ruby27',
-        'go_on_provided',
-        'rust_on_provided_al2',
-        'quarkus_native_on_provided_al2',
-        'dotnet7_aot_on_provided_al2',
+        "nodejs12x",
+        "nodejs14x",
+        "nodejs16x",
+        "nodejs18x",
+        "python37",
+        "python38",
+        "python39",
+        "dotnetcore31",
+        "dotnet6",
+        "go1x",
+        "java11",
+        "java8",
+        "ruby27",
+        "go_on_provided",
+        "rust_on_provided_al2",
+        "quarkus_native_on_provided_al2",
+        "dotnet7_aot_on_provided_al2",
     ];
 
     const s3Client = new S3Client();
 
-    for(const runtime of runtimes) {
+    for (const runtime of runtimes) {
         childProcess.execSync(`./runtimes/${runtime}/build.sh ${runtime}`);
-        const fileStream = fs.createReadStream(`./runtimes/${runtime}/code.zip`);
+        const fileStream = fs.createReadStream(
+            `./runtimes/${runtime}/code.zip`
+        );
         const putObjectParams = {
             Bucket: `lambda-perf-${REGION}`,
             Key: `${runtime}/code.zip`,
             Body: fileStream,
-          };
+        };
         const command = new PutObjectCommand(putObjectParams);
 
         try {
