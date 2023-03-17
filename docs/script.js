d76ee7139688dd485e5ea8bce8c86000a5b07bcb
algo-facile <111239808+algo-facile@users.noreply.github.com>
2023-03-17 10:00:06 -0400

    prevent cache

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 const load = async () => {
-	const request = await fetch('https://raw.githubusercontent.com/maxday/lambda-perf/main/data/last.json?0.45957827529700257')
+	const request = await fetch('https://raw.githubusercontent.com/maxday/lambda-perf/main/data/last.json?0.8906449885026313')
 	const json = await request.json();
 	return json
 }
