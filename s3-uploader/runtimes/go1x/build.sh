436521c2702cab2b54dc3735fb0d5c92948ed84e
Maxime David <maxime.david@datadoghq.com>
2023-04-20 22:52:37 -0400

    add bin bash

--- s3-uploader/runtimes/go1x/build.sh
+++ s3-uploader/runtimes/go1x/build.sh
@@ -8,4 +8,6 @@ rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
 docker build ${DIR_NAME} --build-arg ARCH=${ARCH} -t maxday/go1x
 dockerId=$(docker create maxday/go1x)
+
+ARCH=$2
 docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
--- s3-uploader/runtimes/rust_on_provided_al2/build.sh
+++ s3-uploader/runtimes/rust_on_provided_al2/build.sh
@@ -8,4 +8,5 @@ rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 cd ${DIR_NAME}
 cargo lambda build --release --${ARCH}
 
+ARCH=$2
 zip -j code_${ARCH}.zip target/lambda/lambda-perf/bootstrap
