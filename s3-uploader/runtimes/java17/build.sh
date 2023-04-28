3574f4ef25dea486829939b1b7cba1bbf4e2efec
Maxime David <maxime.david@datadoghq.com>
2023-04-28 09:07:43 -0400

    Add java 17 (#282)
    * add java 17 support
    
    * add java 17 to readme
    
    * remove dotnet3.1
    
    * remove dotnet3.1

--- s3-uploader/runtimes/java17/build.sh
+++ s3-uploader/runtimes/java17/build.sh
@@ -3,6 +3,6 @@ ARCH=$2
 
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-docker build ${DIR_NAME} --build-arg ARCH=${ARCH} -t maxday/dotnetcore31
-dockerId=$(docker create maxday/dotnetcore31)
+docker build ${DIR_NAME} -t maxday/java17
+dockerId=$(docker create maxday/java17)
 docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
