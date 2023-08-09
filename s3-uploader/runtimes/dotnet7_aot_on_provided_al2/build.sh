63cb6d3cdfb866703230f5789dd1e9f0cae5d3f8
Beau Gosse <bgosse@amazon.com>
2023-08-08 23:58:58 -0500

    Add .NET7 AOT ARM (#519)

--- s3-uploader/runtimes/dotnet7_aot_on_provided_al2/build.sh
+++ s3-uploader/runtimes/dotnet7_aot_on_provided_al2/build.sh
@@ -3,6 +3,6 @@ ARCH=$2
 
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-docker build ${DIR_NAME} -t maxday/dotnet7
+docker build ${DIR_NAME} --build-arg ARCH=${ARCH} -t maxday/dotnet7
 dockerId=$(docker create maxday/dotnet7)
 docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
