145c6e354dbd5bd326ae9feb11574959e34dce39
Maxime David <maxime.david@datadoghq.com>
2023-08-09 00:12:26 -0400

    test buildx

--- s3-uploader/runtimes/dotnet7_aot_on_provided_al2/build.sh
+++ s3-uploader/runtimes/dotnet7_aot_on_provided_al2/build.sh
@@ -3,6 +3,7 @@ ARCH=$2
 
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-docker build ${DIR_NAME} --build-arg ARCH=${ARCH} -t maxday/dotnet7
+docker buildx create --name multiarch --platform linux/arm64
+docker buildx build --load --platform linux/arm64  --builder multiarch ${DIR_NAME} --build-arg ARCH=${ARCH} -t maxday/dotnet7
 dockerId=$(docker create maxday/dotnet7)
 docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
