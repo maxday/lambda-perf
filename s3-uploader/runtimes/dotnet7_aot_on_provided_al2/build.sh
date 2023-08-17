861c4111f88841a8b2f407d4642fdd730315151c
Maxime David <maxime.david@datadoghq.com>
2023-08-17 18:38:35 -0400

    reset all

--- s3-uploader/runtimes/dotnet7_aot_on_provided_al2/build.sh
+++ s3-uploader/runtimes/dotnet7_aot_on_provided_al2/build.sh
@@ -3,6 +3,6 @@ ARCH=$2
 
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-docker buildx build --platform linux/${ARCH} --build-arg ARCH=${ARCH} -t maxday/dotnet7 --load ${DIR_NAME}
+docker build ${DIR_NAME} --build-arg ARCH=${ARCH} -t maxday/dotnet7
 dockerId=$(docker create maxday/dotnet7)
 docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
