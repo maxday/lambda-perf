6ecedddcb32d716bca359fa1a28f3e9bfb62cdf6
Maxime David <maxime.david@datadoghq.com>
2023-09-14 11:10:09 -0400

    add bun no layer (#611)

--- s3-uploader/runtimes/bun/build.sh
+++ s3-uploader/runtimes/bun/build.sh
@@ -1,6 +1,17 @@
 DIR_NAME="./runtimes/$1"
 ARCH=$2
 
+if [ $2 = "x86_64" ]; then
+    ARCH_PLATFORM="linux/amd64"
+elif [ $2 = "arm64" ]; then
+    ARCH_PLATFORM="linux/arm64"
+else
+    echo "The process architecture $2 is set incorrectly. The value can only be either x86_64 or arm64."
+    exit 1
+fi
+
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-zip -j ${DIR_NAME}/code_${ARCH}.zip ${DIR_NAME}/index.js
+docker build --platform ${ARCH_PLATFORM} ${DIR_NAME} -t maxday/bun
+dockerId=$(docker create maxday/bun)
+docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
