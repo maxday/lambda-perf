c517e9eb60119e7c7de512c7d2a5e7ab057a3e41
rasvi <mohamed.r@alphacloudlabs.com>
2023-09-22 13:44:02 +0530

    fixed dir name and image name

--- s3-uploader/runtimes/java_graalvm_ce_21_on_provided_al2/build.sh
+++ s3-uploader/runtimes/java_graalvm_ce_21_on_provided_al2/build.sh
@@ -3,6 +3,6 @@ ARCH=$2
 
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-docker build -t java-21-graalvm-on-provided-al2
-dockerId=$(docker create java-21-graalvm-on-provided-al2)
+docker build ${DIR_NAME} -t maxday/java-21-graalvm-on-provided-al2
+dockerId=$(docker create maxday/java-21-graalvm-on-provided-al2)
 docker cp $dockerId:/bootstrap.zip ${DIR_NAME}/code_${ARCH}.zip
