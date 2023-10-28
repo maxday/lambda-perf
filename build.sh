956904704797b88a50842be930370dab61fee019
Maxime David <maxime.david@datadoghq.com>
2023-10-27 22:28:16 -0400

    add rust

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 cargo lambda build --release --arm64 --output-format zip
 
 mv target/lambda/function-trigger-deployer-rs/bootstrap.zip target/lambda/function-trigger-deployer-rs/bootstrap-function-trigger-deployer-rs.zip
-mv target/lambda/function-deployer-image-rs/bootstrap.zip target/lambda/function-deployer-image-rs/bootstrap-function-deployer-image-rs.zip
+# add maniest.json to the zip file
+zip -j target/lambda/function-trigger-deployer-rs/bootstrap-function-trigger-deployer-rs.zip function-trigger-deployer-rs/manifest.json
+
+mv target/lambda/function-image-deployer-rs/bootstrap.zip target/lambda/function-image-deployer-rs/bootstrap-function-image-deployer-rs.zip
