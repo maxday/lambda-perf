d892cfa47b12f8ca9017499ac073404ae83855c7
Maxime David <maxime.david@datadoghq.com>
2023-11-11 01:59:52 -0500

    add provided.al2023 (#749)
    * add provided.al2023
    
    * add provided.al2023
    
    * move
    
    * add provided.al2023 for cpp and rust
    
    * rename swift
    
    * better go
    
    * revert swift

--- s3-uploader/runtimes/swift58_on_provided_al2/build.sh
+++ s3-uploader/runtimes/swift58_on_provided_al2/build.sh
@@ -13,7 +13,7 @@ fi
 
 rm ${DIR_NAME}/code_${2}.zip 2> /dev/null
 
-docker build ${DIR_NAME} --platform ${ARCH} -t maxday/swift_${2}
-dockerId=$(docker create maxday/swift_${2})
+docker build ${DIR_NAME} --platform ${ARCH} -t maxday/swift58_on_provided_al2_${2}
+dockerId=$(docker create maxday/swift58_on_provided_al2_${2})
 
 docker cp $dockerId:/code.zip ${DIR_NAME}/code_${2}.zip
