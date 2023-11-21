f29f9b127615800265cb8b401a1803707d9b914c
Maxime David <maxime.david@datadoghq.com>
2023-11-20 19:51:47 -0500

    bump dependencies

--- function-deployer-rs/src/main.rs
+++ function-deployer-rs/src/main.rs
@@ -24,12 +24,12 @@ struct Response {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
-        .json()
+    tracing_subscriber::fmt().json()
         .with_max_level(tracing::Level::INFO)
-        .with_current_span(false)
         .with_target(false)
         .with_ansi(false)
+        .with_current_span(false)
+        .with_span_list(false)
         .without_time()
         .init();
 
--- function-trigger-deployer-rs/src/main.rs
+++ function-trigger-deployer-rs/src/main.rs
@@ -16,10 +16,12 @@ use sqs_manager::{QueueManager, SQSManager};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
+    tracing_subscriber::fmt().json()
         .with_max_level(tracing::Level::INFO)
         .with_target(false)
         .with_ansi(false)
+        .with_current_span(false)
+        .with_span_list(false)
         .without_time()
         .init();
     let func = service_fn(func);
