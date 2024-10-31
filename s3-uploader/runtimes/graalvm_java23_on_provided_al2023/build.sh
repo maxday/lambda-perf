aa3d72631465a59c9704fa9406bad3ab83d5a2ef
Xavier Bouclet <mikrethor@gmail.com>
2024-10-30 21:18:57 -0400

    Fix build.sh

--- s3-uploader/runtimes/graalvm_java23_on_provided_al2023/build.sh
+++ s3-uploader/runtimes/graalvm_java23_on_provided_al2023/build.sh
@@ -3,6 +3,6 @@ ARCH=$2
 
 rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
 
-docker build ${DIR_NAME} -t maxday/graalvm-java21_on_provided_al2
-dockerId=$(docker create maxday/graalvm-java21_on_provided_al2)
+docker build ${DIR_NAME} -t maxday/graalvm-java23_on_provided_al2023
+dockerId=$(docker create maxday/graalvm-java23_on_provided_al2023)
 docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
