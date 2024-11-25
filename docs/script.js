5ec5081a8db1284fcd356aa284e8d30d1a68ca5c
algo-facile <111239808+algo-facile@users.noreply.github.com>
2024-11-25 14:00:15 +0000

    prevent cache

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,7 @@ const dataManager = {
 
 const load = async (dataManager) => {
   const request = await fetch(
-    "https://raw.githubusercontent.com/maxday/lambda-perf/main/data/last.json?0.7801795517575556"
+    "https://raw.githubusercontent.com/maxday/lambda-perf/main/data/last.json?0.6777065742488686"
   );
   const json = await request.json();
   dataManager.fetchData = json;
