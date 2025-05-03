e216495d0823fdfcd913403895dca02fa98e3ff4
Maxime David <contact@maxday.dev>
2025-05-03 11:53:05 +0100

    feat: add ruby3.4

---
+++ s3-uploader/runtimes/ruby34/build.sh
@@ -0,0 +1,6 @@
+DIR_NAME="./runtimes/$1"
+ARCH=$2
+
+rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
+
+zip -j ${DIR_NAME}/code_${ARCH}.zip ${DIR_NAME}/handler.rb
---
+++ s3-uploader/runtimes/ruby34/handler.rb
@@ -0,0 +1,3 @@
+def handler(event:, context:)
+    { statusCode: 200 }
+end
