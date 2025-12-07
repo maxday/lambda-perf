4b5021c6f92d9f3625f94f465dbfe285b6657d96
Maxime David <contact@maxday.dev>
2025-12-06 22:18:07 -0500

    feat: remove deprecated runtimes

--- s3-uploader/runtimes/dotnet6/build.sh
+++
@@ -1,8 +0,0 @@
-DIR_NAME="./runtimes/$1"
-ARCH=$2
-
-rm ${DIR_NAME}/code_${ARCH}.zip 2> /dev/null
-
-docker build ${DIR_NAME} --build-arg ARCH=${ARCH} -t maxday/dotnet6
-dockerId=$(docker create maxday/dotnet6)
-docker cp $dockerId:/code.zip ${DIR_NAME}/code_${ARCH}.zip
--- s3-uploader/runtimes/python38/index.py
+++
@@ -1,4 +0,0 @@
-def handler(event, context):
-    return {
-        'statusCode': 200,
-    }
